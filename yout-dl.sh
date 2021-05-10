5280a638034878ecb6b5227b7ad9eeef38ba3b3e
Boyd G <tech2me.bg@gmail.com>
2021-05-10 06:27:08 -0700

    Update yout-dl.sh

--- yout-dl.sh
+++ yout-dl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#Make this into your own terminal call command for other systems, make sure to change the path for your script placment.
 gnome-terminal -- sh /home/s/.bash/bin/download.sh
