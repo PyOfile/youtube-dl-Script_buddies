80c2e1263181a4520125cc44bdd007df7638b7d2
Boyd G <tech2me.bg@gmail.com>
2021-05-10 06:30:16 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+# prompts for URL inpute for video you like that is not DRM protected like movies.
 read -p "Download-URL: " t
-
+# launches the star here youtube-dl with the URL that was used
 youtube-dl "$t"
-
+#Closing message, changed to what ever or leave off
 echo "Cool Beans!"
-
+# So you can see the message or errors before terminal closes
 sleep 10
