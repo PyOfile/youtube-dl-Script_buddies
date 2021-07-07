7ef84861c9b3f1a5148c02914a52d48d75b15322
Boyd G <tech2me.bg@gmail.com>
2021-07-07 09:22:03 -0700

    Add files via upload

--- yout-dl.sh
+++ yout-dl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Make this into your own terminal call command for other systems, make sure to change the path for your script placment.
-gnome-terminal -- sh /home/user/.bash/bin/download.sh
+
+gnome-terminal -- sh /home/s/.bash/bin/you_launch.sh
