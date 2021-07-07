43fdba47715d777848ef722422d1788fd7ba46c6
Boyd G <tech2me.bg@gmail.com>
2021-07-07 09:21:22 -0700

    Add files via upload

---
+++ you_launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Enter your choice [A] audio [V] video: " choice
+ 
+case $choice in
+     A)
+          echo "You Want Audio Format!"
+          sh ~/.bash/bin/download.sh
+          ;;
+     B)
+          echo "You Want Video Format!"
+          sh ~/.bash/bin/download0.sh
+          ;;
+     *)
+          echo "Sorry, invalid input"
+          ;;
+esac
