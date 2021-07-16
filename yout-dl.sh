23ad7dd4791d718826ed4ec6613c4474d0f3da28
Boyd G <contact@webcrack.tech>
2021-07-16 06:32:10 -0700

    Update yout-dl.sh

--- yout-dl.sh
+++ yout-dl.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# I use this script with an i3 keybind. So the purpose is to open a terminal for the rest of the scripts to run.
 
 gnome-terminal -- sh /home/user/.bash/bin/you_launch.sh
