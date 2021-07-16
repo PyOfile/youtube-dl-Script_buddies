8b4d32c475ccddd24b610b85d343f81c25c722e0
Boyd G <contact@webcrack.tech>
2021-07-16 06:37:48 -0700

    Update download0.sh

--- download0.sh
+++ download0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script will download the video you URL you inpute when asked.
+# This script will download the video from the URL you inpute when asked.
 
 read -p "Download-URL: " t
 
